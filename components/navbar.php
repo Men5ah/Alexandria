da4a85d98130056c65753a165d65fceb4decb117
Men5ah <96393336+Men5ah@users.noreply.github.com>
2024-04-05 00:53:42 +0000

    Initial project set up

---
+++ components/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="navbar-brand">
+            <a href="#" class="navbar-logo">Alexandria</a>
+        </div>
+        <ul class="navbar-menu">
+            <li class="navbar-item"><a href="recently_read.html" class="navbar-link">Recently Read</a></li>
+            <li class="navbar-item"><a href="search.html" class="navbar-link">Search</a></li>
+            <li class="navbar-item"><a href="reading_lists.html" class="navbar-link">Reading Lists</a></li>
+            <li class="navbar-item"><a href="offline_reading.html" class="navbar-link">Offline Reading</a></li>
+            <li class="navbar-item"><a href="profile.html" class="navbar-link">Profile</a></li>
+            <li class="navbar-item"><a href="settings.html" class="navbar-link">Settings</a></li>
+            <li class="navbar-item"><a href="help.html" class="navbar-link">Help/Support</a></li>
+        </ul>
+    </div>
+</nav>
