9489842885c35451552ca3061566de7c34ce20c2
Men5ah <96393336+Men5ah@users.noreply.github.com>
2024-04-07 01:13:38 +0000

    Files for reading list created

--- components/navbar.php
+++ components/navbar.php
@@ -6,7 +6,7 @@
         <ul class="navbar-menu">
             <li class="navbar-item"><a href="#" class="navbar-link">Dashboard</a></li>
             <li class="navbar-item"><a href="../views/search.php" class="navbar-link">Search</a></li>
-            <li class="navbar-item"><a href="#" class="navbar-link">Reading Lists</a></li>
+            <li class="navbar-item"><a href="../views/readinglist.php" class="navbar-link">Reading Lists</a></li>
             <li class="navbar-item"><a href="#" class="navbar-link">Offline Reading</a></li>
             <li class="navbar-item"><a href="#" class="navbar-link">Profile</a></li>
             <li class="navbar-item"><a href="#" class="navbar-link">Settings</a></li>
